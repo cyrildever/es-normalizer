d2172291bff52807b63881c596f1cd8e44846480
Cyril Dever <cdever@edgewhere.fr>
2020-01-24 14:32:59 +0100

    add translateText() method to Dictionary

--- lib/src/typescript/Dictionary/index.ts
+++ lib/src/typescript/Dictionary/index.ts
@@ -1,17 +1,24 @@
 export interface Dictionary {
-  set: {[key: string]: string}
+  set: { [key: string]: string }
+  translateText: (input: string) => string
   translateWord: (input: string) => string
 }
 
-const translateWord = (set: {[key: string]: string}) => (input: string): string =>
+const translateText = (set: { [key: string]: string }) => (input: string): string =>
+  input.split(' ')
+    .map((word: string) => translateWord(set)(word))
+    .join(' ')
+
+const translateWord = (set: { [key: string]: string }) => (input: string): string =>
   set[input] || input
 
-export const Dictionary = (set: {[key: string]: string}): Dictionary => ({
+export const Dictionary = (set: { [key: string]: string }): Dictionary => ({
   set,
+  translateText: translateText(set),
   translateWord: translateWord(set)
 })
 
-export const getSet = (data:string) =>
+export const getSet = (data: string) =>
   Object.fromEntries(data.split(/\n/g).map(line => line.split(/\t/g, 2)))
 
 export * from './firstname.dico'
--- lib/src/typescript/Email.ts
+++ lib/src/typescript/Email.ts
@@ -1,11 +1,10 @@
 import { Maybe, None, Some } from 'monet'
 
 export const Email = (data: string): Maybe<string> => {
-    const re = RegExp(/(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*)@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?:[a-z]{2,}[a-z0-9-]*|([a-z][a-z0-9-]+))|(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.?){4})/)
-    const processed = data.trim().toLowerCase()
-    if (processed.length > 255 || !re.test(processed)) {
-      return None<string>()
-    }
-    return Some(processed)
+  const re = RegExp(/(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*)@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?:[a-z]{2,}[a-z0-9-]*|([a-z][a-z0-9-]+))|(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.?){4})/)
+  const processed = data.trim().toLowerCase()
+  if (processed.length > 255 || !re.test(processed)) {
+    return None<string>()
   }
-  
+  return Some(processed)
+}
